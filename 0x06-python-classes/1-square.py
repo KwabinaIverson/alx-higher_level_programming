e2af0c56744032877dfdd9181b1854f07baed524
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-08-29 16:36:38 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class that represents a square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a Square object.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
