d813e9dcd41d257bf1316f44577dd058cda01be5
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-09-04 17:18:06 -0700

    Attribute

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -17,6 +17,7 @@ class Rectangle:
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
         """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
     
