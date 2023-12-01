e3c9edb89297d55735daba098cb0744306433f8f
Benjamin Atsu <Bredrenbonish@gmail.com>
2023-12-01 09:20:07 -0800

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """ HTTP fetch request from a url """
-    import urllib.request
+""" HTTP fetch request from a url """
+import urllib.request
  
-    url = 'https://alx-intranet.hbtn.io/status'
+url = 'https://alx-intranet.hbtn.io/status'
 
-    # Make the request
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
+# Make the request
+with urllib.request.urlopen(url) as response:
+    body = response.read()
 
-    # Response print format
-    print("Body response:$")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", body.decode('utf-8'))
+# Response print format
+print("Body response:$")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", body.decode('utf-8'))
